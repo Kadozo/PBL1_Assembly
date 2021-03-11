0db19ecf943293d5868e73a1f6a7ec8bcc238437
Mateus Fernandes Santos <79941524+MatFSantos@users.noreply.github.com>
2021-03-11 18:26:16 -0300

    Update main.c

--- files/meta 3/main.c
+++ files/meta 3/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- int soma( int a, int b, int c);
+ int mult( int a, int b);
 
 
  int main() {
-	 int x, y, z;
-	scanf("%d %d %d", &x, &y, &z);
-	printf("resultado = %d\n", soma(x, y, z));
+	 int x, y;
+	scanf("%d %d %d", &x, &y);
+	printf("resultado = %d\n", soma(x, y));
 }
 
- int soma( int a,  int b, int c) {
-	 int d;
+ int soma( int a,  int b) {
+	 int c;
 	__asm__(
 		"imul %1, %2"
-		:[saida] "=r" (d)
-		: "r" (a),"r" (b), "r" (c)
+		:[saida] "=r" (c)
+		: "r" (a),"r" (b)
 	);
-	return d;
+	return c;
 }
