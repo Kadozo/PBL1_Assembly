b2797fd7978f3d869d6392769c34a05f31278d40
Thiago <cardozothiago07@gmail.com>
2021-03-12 11:17:55 -0300

    rename folder

---
+++ files/Metas 3/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int mult( int a, int b);
+
+
+ int main() {
+	 int x, y;
+	scanf("%d %d", &x, &y);
+	printf("resultado = %d\n", mult(x, y));
+}
+
+ int mult( int a,  int b) {
+	 int c;
+	__asm__(
+		"mul %2\n"
+		"mov %[saida],%%eax"
+		:[saida] "=r" (c)
+		: "r" (a),"r" (b)
+	);
+	return c;
+}
