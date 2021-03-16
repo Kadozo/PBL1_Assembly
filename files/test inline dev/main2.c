390ce41e871ca44240b61f6300f3bbff11eff67a
Mateus Fernandes Santos <79941524+MatFSantos@users.noreply.github.com>
2021-03-16 17:20:35 -0300

    Add files via upload

--- files/test inline dev/main2.c
+++ files/test inline dev/main2.c
@@ -4,36 +4,42 @@
 
 
 int main(){
-	int c = 0, a = 1000, i = 0;
-	int aux1 = 1, aux2 = 2;
+	int c = 0, a = 2, i = 0;
+	//int aux1 = 1, aux2 = 2;
 	
 	/*
 	__asm__(
 		"l1:\n"
-		"cmp %1, %2\n"
-		"jge l1end\n"
-		"mov %3, %0\n"
-		"jmp l1end2\n"
+			"cmp %1, %2\n"
+			"jge l1end\n"
+			"mov %3, %0\n"
+			"jmp l1end2\n"
 		"l1end:\n"
-		"mov %4, %0\n"
+			"mov %4, %0\n"
 		"l1end2:\n"
 		: [saida] "+r" (c)
 		: "r" (a), "r" (i), "r" (aux1), "r" (aux2)
 	); 
 	*/
-	
-	
-	// /*
+	/*
 	__asm__(
 		"l1:\n"
-		"cmp %1, %2\n"
-		"jge l1end\n"
-		"inc %[saida]\n"
-		"inc %2\n"
-		"jmp l1\n"
+			"cmp %1, %2\n"
+			"jge l1end\n"
+			"inc %[saida]\n"
+			"inc %2\n"
+			"jmp l1\n"
 		"l1end:\n"
 		: [saida] "+r" (c)
 		: "r" (a), "r" (i), "r" (aux1), "r" (aux2)
+	);
+	*/
+	
+	// /*
+	__asm__(
+		"add %1, %[saida]\n"
+		: [saida] "+r" (c)
+		: "r" (a)
 	);
 	
 	//*/
